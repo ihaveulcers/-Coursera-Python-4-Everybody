0a0d426db642c919c5ad3b12dce6754b22f76ccb
ihaveulcers <43128447+ihaveulcers@users.noreply.github.com>
2018-10-26 08:06:55 +0800

    .

--- PY4E_Course4_Week 2.py
+++ PY4E_Course4_Week 2.py
@@ -1,32 +1,35 @@
+
 import sqlite3
 
-connection = sqlite3.connect('cody.sqlite')
-cur = connection.cursor()
+conn = sqlite3.connect('cody.sqlite')
+cur = conn.cursor()
+
 
 cur.execute('DROP TABLE IF EXISTS Counts')
-cur.execute('CREATE TABLE COUNTS (org TEXT, count INTEGER)')
 
-fname = input('Enter file name:')
-if len(fname) < 1:
-    fname = 'mbox.txt'
+cur.execute('CREATE TABLE Counts (org TEXT, count INTEGER)')
+
+fname = input('Enter file name: ')
+if len(fname) < 1:    fname = 'mbox.txt'
 fh = open(fname)
 for line in fh:
-    if not line.startswith('From: '):
-        continue
+    if not line.startswith('From: '): continue
     pieces = line.split()
     email = pieces[1]
     org = email.split('@')[1]
-    cur.execute('SELECT count FROM Counts WHERE org = ?',(org,))
-    try :
+    cur.execute('SELECT count FROM Counts WHERE org = ?', (org, ))
+
+    try:
         count = cur.fetchone()[0]
-        cur.execute('UPDATE Counts SET count = count + 1 WHERE org = ?',(org,))
-    except :
-        cur.execute('INSERT INTO Counts (org,count) VALUES (?,1)'(org,))
+        cur.execute('UPDATE Counts SET count = count + 1 WHERE org = ?', (org, ))
+    except:
+        cur.execute('INSERT INTO Counts (org, count) VALUES (?, 1)', (org, ))
 
-connection.commit()
+conn.commit()
 
-sqlstr = 'SELECT org,count FROM Counts ORDER BY count DESC LIMIT 10'
-for row in connection.execute(sqlstr):
-    print(row[0],row[1])
+sqlstr = 'SELECT org, count FROM Counts ORDER BY count DESC LIMIT 10'
 
-connection.close()
+for row in conn.execute(sqlstr):
+    print(row[0], row[1])
+
+conn.close()
